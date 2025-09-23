2ace33e404b8c8b1865d4607c2c6d6f0448e7454
Pravallika8143 <mamidisettipravallika4@gmail.com>
2025-09-23 14:21:10 +0530

    categories

--- src/Categories.jsx
+++ src/Categories.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function Categories() {
   let [categories, setCategories] = useState([]);
   useEffect(() => {
-    document.title = "Categories";
+    document.title = "Categories"
   }, []);
   useEffect(() => {
     fetch('https://dummyjson.com/products/categories')
